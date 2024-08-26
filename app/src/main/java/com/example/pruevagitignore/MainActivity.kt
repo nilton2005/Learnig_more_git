0454464cca31d8fd20f0ce651ab8a239609812dd
Nilton taco <nilton2005marzo@gmail.com>
2024-08-26 14:32:32 -0500

    feat(view): add method text field

--- app/src/main/java/com/example/pruevagitignore/MainActivity.kt
+++ app/src/main/java/com/example/pruevagitignore/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.pruevagitignore.ui.theme.PruevaGitignoreTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,8 +31,10 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+                    GeetingTexField("nilton")
                 }
             }
+
         }
     }
 }
@@ -36,6 +45,18 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+}
+// Esto será el priemro cambio de prueba
+@Composable
+fun GeetingTexField(
+    name: String, modifier: Modifier = Modifier){
+    var mutableValue by remember {mutableStateOf("")}
+    Column(
+        modifier = modifier
+            .padding(30.dp)
+    ){
+        TextField(value = mutableValue, onValueChange = {mutableValue = it})
+    }
 }
 
 @Preview(showBackground = true)
