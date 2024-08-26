8012d9b5d32f63043aa650435921fa224526287c
Nilton taco <nilton2005marzo@gmail.com>
2024-08-26 18:08:09 -0500

    feat(view): add method Snackbar

--- app/src/main/java/com/example/pruevagitignore/MainActivity.kt
+++ app/src/main/java/com/example/pruevagitignore/MainActivity.kt
@@ -4,17 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,6 +37,7 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.padding(innerPadding)
                     )
                     GeetingTexField("nilton")
+                    SnackbarEjemplo()
                 }
             }
 
@@ -58,6 +64,42 @@ fun GeetingTexField(
         TextField(value = mutableValue, onValueChange = {mutableValue = it})
     }
 }
+
+
+
+
+@Composable
+fun SnackbarEjemplo() {
+    var showSnackbar by remember {
+        mutableStateOf(
+            false
+        )
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { showSnackbar = true }) {
+            Text("Mostrar Snackbar")
+        }
+
+        if (showSnackbar) {
+            Snackbar(
+                action = {
+                    TextButton(onClick = { showSnackbar = false }) {
+                        Text("OK")
+                    }
+                },
+                modifier = Modifier.padding(8.dp)
+            ) {
+                Text("¡Hola desde el Snackbar!")
+            }
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
